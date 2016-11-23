c20f93b1756406c81b2a6bdc3d030f5c980f6419
Clark <nickclark1000@gmail.com>
2016-11-23 16:51:20 -0400

    Handle the situation where sprint iteration ids are not in ascending order according to their names

--- R/release_summary.R
+++ R/release_summary.R
@@ -1,9 +1,11 @@
 #for data wrangling
 library(dplyr)
-#for calculating moving averages
-library(TTR)
 #for tfs data
 library(rtfs)
+#for handling dates
+library(lubridate)
+#for calculating moving average
+source("library.R")
 
 
 current_release <- rtfs::get_current_release()
@@ -17,8 +19,12 @@ if (is.null(current_minor_release)) {
 
 release_summary <- data.frame(TEAM = tfs_team, RELEASE_ITERATION_ID = current_release$id, 
     RELEASE_NAME = current_release$name, SPRINT_ITERATION_ID = current_release$children[[1]]$id, 
-    SPRINT_NAME = current_release$children[[1]]$name, SPRINT_INDEX = c(1:nrow(current_release$children[[1]])), 
+    SPRINT_NAME = current_release$children[[1]]$name,  
     START_DATE = current_release$children[[1]]$attributes$startDate, END_DATE = current_release$children[[1]]$attributes$finishDate)
+
+release_summary <- release_summary[order(as.Date(release_summary$START_DATE)),]
+
+release_summary$SPRINT_INDEX <- c(1:nrow(current_release$children[[1]]))
 
 today <- format(Sys.Date())
 for (i in 1:nrow(release_summary)) {
@@ -26,7 +32,7 @@ for (i in 1:nrow(release_summary)) {
         today <= as.Date(release_summary$END_DATE[i])) 
         CURRENT_SPRINT_INDEX <<- release_summary$SPRINT_INDEX[i]
 }
-#release_summary <- subset(release_summary, SPRINT_INDEX < CURRENT_SPRINT_INDEX)
+release_summary <- subset(release_summary, SPRINT_INDEX < CURRENT_SPRINT_INDEX)
 
 iteration_ids <- paste(c(current_release$id, as.character(release_summary$SPRINT_ITERATION_ID)), 
     collapse = ",")
@@ -35,7 +41,7 @@ work_item_ids <- rtfs::get_release_wi_ids(iteration_ids)$content
 
 work_item_df <- rtfs::get_release_wis(work_item_ids$workItems$id)
 
-# Add Velocity
+# Add Actual Velocity
 release_summary <- inner_join(release_summary, 
                                  rename(work_item_df %>% 
                                  group_by(System.IterationId) %>% 
@@ -43,17 +49,22 @@ release_summary <- inner_join(release_summary,
                                  SPRINT_ITERATION_ID = System.IterationId), 
                               by = "SPRINT_ITERATION_ID")
 
+# Add Planned Velocity
+planned_velocity <- data.frame(PLANNED_VELOCITY = double(),
+                               SPRINT_ITERATION_ID = integer())
+for(i in 1:length(release_summary$SPRINT_ITERATION_ID)){
+  iteration_id <- release_summary$SPRINT_ITERATION_ID[i]
+  date_time <- update(as_datetime(release_summary$START_DATE[i]), hour = 23, minute = 59)
+  result <- rtfs::get_planned_velocity(iteration_id, date_time)
+  planned_velocity <- bind_rows(planned_velocity, result)
+}
+release_summary <- inner_join(release_summary, planned_velocity, by = "SPRINT_ITERATION_ID")
+
 ### Total work done measured at the END of each sprint
 release_summary$COMPLETED_RELEASE_POINTS <- cumsum(release_summary$VELOCITY)
 
 ### Moving average calculations
-if (nrow(release_summary) > 4) {
-    release_summary$VELOCITY_SMA_5 <- SMA(release_summary$VELOCITY, 5)
-    release_summary$VELOCITY_WMA_5 <- WMA(release_summary$VELOCITY, 5)
-} else {
-    release_summary$VELOCITY_SMA_5 <- SMA(release_summary$VELOCITY, nrow(release_summary) - 1)
-    release_summary$VELOCITY_WMA_5 <- WMA(release_summary$VELOCITY, nrow(release_summary) - 1)
-}
+release_summary$VELOCITY_MA_5 <- velocity_moving_average(release_summary$VELOCITY)
 
 ### Defects summary
 release_summary <- left_join(release_summary, 
@@ -87,3 +98,6 @@ release_summary <- left_join(release_summary,
 backlog_history <- rtfs::get_backlog_history(iteration_ids, release_summary$END_DATE)
 
 release_summary <- left_join(release_summary, backlog_history, by = c(END_DATE = "AS_OF"))
+
+#test_case_history <- rtfs::get_tc_automation_history(release_summary$END_DATE)
+#release_summary <- left_join(release_summary, test_case_history, by = c(END_DATE = "AS_OF"))
