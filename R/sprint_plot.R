810f84590abf7c278155d069bfe79c0db1d3d092
Clark <nickclark1000@gmail.com>
2016-12-19 16:16:31 -0400

    Changed to Day of Sprint (remove weekends as well)

--- R/sprint_plot.R
+++ R/sprint_plot.R
@@ -1,9 +1,9 @@
 plotSprintChart <- function(df, chartTitle, yAxisTitle){
+  df$SPRINT_DAY <- c(1:nrow(df))
+#  df$X_LABEL <- df %>% tidyr::unite(label, SPRINT_DAY, Date, sep = ' - ')
   long_df <- df %>% tidyr::gather(Type, Points, c(Completed, Total))
-  lastDateUnix <- as.numeric(as.POSIXct(tail(df$Date, 1)))
-  
-  plot_ly(data = long_df, x = as.Date(Date), y = Points, color = Type, fill = 'tonexty') %>%
+  plot_ly(data = long_df, x = SPRINT_DAY, y = Points, color = Type, fill = 'tonexty') %>%
   layout(title = chartTitle, 
          yaxis = list(title = yAxisTitle, rangemode = "tozero"), 
-         xaxis = list(title = "Date", type = "date"))
+         xaxis = list(title = "Day of Sprint", autotick = FALSE))
 }
