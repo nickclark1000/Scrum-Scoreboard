0fb804f549b275c850f4e2c040ea3bf65895a8d3
Clark <nickclark1000@gmail.com>
2017-02-28 14:50:51 -0400

    changes to support plotly v4

--- R/sprint_plot.R
+++ R/sprint_plot.R
@@ -1,9 +1,14 @@
 plotSprintChart <- function(df, chartTitle, yAxisTitle){
   df$SPRINT_DAY <- c(1:nrow(df))
-#  df$X_LABEL <- df %>% tidyr::unite(label, SPRINT_DAY, Date, sep = ' - ')
   long_df <- df %>% tidyr::gather(Type, Points, c(Completed, Total))
-  plot_ly(data = long_df, x = SPRINT_DAY, y = Points, color = Type, fill = 'tonexty') %>%
-  layout(title = chartTitle, 
-         yaxis = list(title = yAxisTitle, rangemode = "tozero"), 
-         xaxis = list(title = "Day of Sprint", autotick = FALSE))
+  plot_ly(long_df, 
+          x = ~SPRINT_DAY, 
+          y = ~Points, 
+          color = ~Type, 
+          type = 'scatter', 
+          mode = 'lines+markers', 
+          fill = 'tonexty') %>%
+      layout(title = ~chartTitle, 
+             yaxis = list(title = ~yAxisTitle, rangemode = "tozero"), 
+             xaxis = list(title = "Day of Sprint", autotick = FALSE))
 }
--- R/velocity_plot.R
+++ R/velocity_plot.R
@@ -1,11 +1,14 @@
 plotVelocityTimeSeries <- function(sprint, actualVelocity, plannedVelocity, movingAverage){
-
-  df<-data.frame(Sprint=as.factor(sprint), Actual = actualVelocity, Planned = plannedVelocity, Average = movingAverage)
+  df <- data.frame(Sprint = as.factor(sprint), Actual = actualVelocity, Planned = plannedVelocity, Average = movingAverage)
   long_df <- df %>% tidyr::gather(Type, Velocity, c(Actual, Planned, Average))
-  p<-plot_ly(data = long_df, x = Sprint, y = Velocity, color = Type) %>%
-  layout(title = "Velocity", yaxis = list(rangemode = "tozero", title = "Story Points"), xaxis = list(autotick = FALSE, dtick = 1), legend = list(x = 0.5, y = 0))
-  
-  # Sys.setenv("plotly_username" = "nickclark1000")
-  # Sys.setenv("plotly_api_key" = "hwwklug3c1")
-  # plotly_IMAGE(p, format = "png", out_file = "output.png")
+  plot_ly(long_df, 
+          x = ~Sprint, 
+          y = ~Velocity, 
+          color = ~Type, 
+          type = 'scatter', 
+          mode = 'lines+markers+text') %>%
+      layout(title = "Velocity", 
+             yaxis = list(rangemode = "tozero", title = "Story Points"), 
+             xaxis = list(autotick = FALSE, dtick = 1), 
+             legend = list(x = 0.5, y = 0))
 }
